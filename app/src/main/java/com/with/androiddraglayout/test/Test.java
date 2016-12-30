4ffb0550b8253084fd989416e36a8a62855033af
with1993 <1490249589@qq.com>
2016-12-30 16:23:43 +0800

    Initial commit

---
+++ app/src/main/java/com/with/androiddraglayout/test/Test.java
@@ -0,0 +1,19 @@
+package com.with.androiddraglayout.test;
+
+
+import com.with.androiddraglayout.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+
+public class Test extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test);
+	}
+
+}
